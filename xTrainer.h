5e51155fc94ab71ca7c92cfbea81ac41a364a53d
Jordan Richards <shadow13epic@gmail.com>
2018-01-12 13:05:12 -0500

    Added install target to CMake

---
+++ xTrainer.h
@@ -0,0 +1,14 @@
+/*
+ * xTrainer.hpp
+ *
+ *  Created on: Jan 12, 2018
+ *      Author: shado
+ */
+
+#ifndef XTRAINER_H_
+#define XTRAINER_H_
+
+#include "trainer/trainer.h"
+#include "tools/tools.h"
+
+#endif /* XTRAINER_H_ */
